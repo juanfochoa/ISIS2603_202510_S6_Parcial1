98fd4fedc4348af265870d65bbfa8c3bc3b80d90
Juan Felipe Ochoa Bejarano <jf.ochoa@uniandes.edu.co>
2025-03-07 14:24:20 -0500

    Creación relación Ruta y Estacion

--- src/main/java/co/edu/uniandes/dse/parcial1/entities/EstacionEntity.java
+++ src/main/java/co/edu/uniandes/dse/parcial1/entities/EstacionEntity.java
@@ -1,7 +1,13 @@
 package co.edu.uniandes.dse.parcial1.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
@@ -9,4 +15,8 @@ public class EstacionEntity extends BaseEntity {
     private String name;
     private String direccion;
     private Integer capacidad;
+
+    @PodamExclude
+    @OneToMany(fetch= FetchType.LAZY)
+    private List<RutaEntity> rutas = new ArrayList<>();
 }
--- src/main/java/co/edu/uniandes/dse/parcial1/entities/RutaEntity.java
+++ src/main/java/co/edu/uniandes/dse/parcial1/entities/RutaEntity.java
@@ -1,7 +1,13 @@
 package co.edu.uniandes.dse.parcial1.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
@@ -11,4 +17,7 @@ public class RutaEntity extends BaseEntity {
     private String color;
     private String tipo;
 
+    @PodamExclude
+    @OneToMany(fetch= FetchType.LAZY)
+    private List<EstacionEntity> estaciones = new ArrayList<>();
 }
